404ccd852151f439daab7171099a67148e43e281
KEWIN Drgas <kewindrgas@MacBook-Pro-KEWIN.local>
2023-02-18 12:52:12 +0100

    amendments in syntax

--- src/App.vue
+++ src/App.vue
@@ -1,154 +1,154 @@
 <template>
-	<nav>
-		<the-nav></the-nav>
-	</nav>
-	<main>
-		<router-view></router-view>
-	</main>
-	<footer>
-		<the-footer></the-footer>
-	</footer>
+  <nav>
+    <the-nav></the-nav>
+  </nav>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    <the-footer></the-footer>
+  </footer>
 </template>
 
 <script>
-import TheNav from "./components/nav/nav.vue";
-import TheFooter from "./components/footer/footer.vue";
+import TheNav from './components/nav/nav.vue';
+import TheFooter from './components/footer/footer.vue';
 
 export default {
-	components: {
-		TheNav,
-		TheFooter,
-	},
+  components: {
+    TheNav,
+    TheFooter,
+  },
 
-	data() {
-		return {
-			mainePageOffers: [
-				{
-					title: "Demo",
-					price: "$0",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja poza pakietem", status: false },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-				{
-					title: "Standard",
-					price: "$99",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja w pakiecie", status: true },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-				{
-					title: "Premium",
-					price: "$299",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja w pakiecie", status: true },
-						feature4: { content: "Opcja w pakiecie", status: true },
-						feature5: { content: "Opcja w pakiecie", status: true },
-					},
-				},
-			],
-			allOffers: [
-				{
-					title: "Demo",
-					price: "$0",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja poza pakietem", status: false },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-				{
-					title: "Standard",
-					price: "$99",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja w pakiecie", status: true },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-				{
-					title: "Premium",
-					price: "$299",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja w pakiecie", status: true },
-						feature4: { content: "Opcja w pakiecie", status: true },
-						feature5: { content: "Opcja w pakiecie", status: true },
-					},
-				},
-				{
-					title: "Demo2",
-					price: "$0",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja poza pakietem", status: false },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-				{
-					title: "Demo3",
-					price: "$0",
-					features: {
-						feature1: { content: "Opcja w pakiecie", status: true },
-						feature2: { content: "Opcja w pakiecie", status: true },
-						feature3: { content: "Opcja poza pakietem", status: false },
-						feature4: { content: "Opcja poza pakietem", status: false },
-						feature5: { content: "Opcja poza pakietem", status: false },
-					},
-				},
-			],
-		};
-	},
-	provide() {
-		return {
-			mainePageOffers: this.mainePageOffers,
-			allOffers: this.allOffers,
-		};
-	},
+  data() {
+    return {
+      mainePageOffers: [
+        {
+          title: 'Demo',
+          price: 0,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+        {
+          title: 'Standard',
+          price: 99,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+        {
+          title: 'Premium',
+          price: 299,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+          ],
+        },
+      ],
+      allOffers: [
+        {
+          title: 'Demo',
+          price: 0,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+        {
+          title: 'Standard',
+          price: 99,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+        {
+          title: 'Premium',
+          price: 299,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+          ],
+        },
+        {
+          title: 'Demo2',
+          price: 0,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+        {
+          title: 'Demo3',
+          price: 0,
+          features: [
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja w pakiecie', status: true },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+            { content: 'Opcja poza pakietem', status: false },
+          ],
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      mainePageOffers: this.mainePageOffers,
+      allOffers: this.allOffers,
+    };
+  },
 };
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
 
 *,
 *::before,
 *::after {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 html {
-	font-size: 62.5%;
-	scroll-behavior: smooth;
-	scroll-padding: 8em;
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  scroll-padding: 8em;
 }
 
 body {
-	font-size: 1.6rem;
-	font-family: "Montserrat", sans-serif;
+  font-size: 1.6rem;
+  font-family: 'Montserrat', sans-serif;
 }
 
 section {
-	padding-bottom: 6rem;
+  padding-bottom: 6rem;
 }
 
 p,
@@ -158,72 +158,72 @@ h3,
 h4,
 h5,
 h6 {
-	line-height: 180%;
-	margin: 0;
+  line-height: 180%;
+  margin: 0;
 }
 
 nav {
-	position: sticky;
-	top: 0;
-	padding: 0;
-	z-index: 100;
+  position: sticky;
+  top: 0;
+  padding: 0;
+  z-index: 100;
 }
 
 .section-title {
-	text-transform: uppercase;
-	font-size: 3.5rem;
-	font-weight: 200;
-	text-align: center;
-	margin-bottom: 2rem;
+  text-transform: uppercase;
+  font-size: 3.5rem;
+  font-weight: 200;
+  text-align: center;
+  margin-bottom: 2rem;
 }
 
 .special-link {
-	position: relative;
-	text-decoration: none;
-	color: #000;
-	z-index: 2;
+  position: relative;
+  text-decoration: none;
+  color: #000;
+  z-index: 2;
 }
 
 .special-link::before {
-	content: "";
-	position: absolute;
-	bottom: -4px;
-	left: -2px;
-	width: calc(100% + 4px);
-	height: 3px;
-	background-color: rgb(0, 221, 118);
-	transition: height 0.4s;
-	z-index: -1;
+  content: '';
+  position: absolute;
+  bottom: -4px;
+  left: -2px;
+  width: calc(100% + 4px);
+  height: 3px;
+  background-color: rgb(0, 221, 118);
+  transition: height 0.4s;
+  z-index: -1;
 }
 
 .special-link:hover::before {
-	height: calc(100% + 8px);
+  height: calc(100% + 8px);
 }
 
 .container {
-	width: 100vw;
-	margin: 0 auto;
-	padding: 0 1.5rem;
+  width: 100vw;
+  margin: 0 auto;
+  padding: 0 1.5rem;
 }
 
 @media (min-width: 768px) {
-	.container {
-		width: 720px;
-	}
+  .container {
+    width: 720px;
+  }
 }
 @media (min-width: 992px) {
-	.container {
-		width: 960px;
-	}
+  .container {
+    width: 960px;
+  }
 }
 @media (min-width: 1200px) {
-	.container {
-		width: 1140px;
-	}
+  .container {
+    width: 1140px;
+  }
 }
 @media (min-width: 1400px) {
-	.container {
-		width: 1320px;
-	}
+  .container {
+    width: 1320px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,66 +1,62 @@
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import App from "./App.vue";
+import App from './App.vue';
 
 //fontAwesome
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 library.add(fas);
 
 //pages
 
-import ContactPage from "./components/pages/contact-page.vue";
-import MainPage from "./components/pages/main-page.vue";
-import OfferPage from "./components/pages/offer-page.vue";
+import ContactPage from './components/pages/contact-page.vue';
+import MainPage from './components/pages/main-page.vue';
+import OfferPage from './components/pages/offer-page.vue';
 
 // router
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes: [
-		{
-			path: "/",
-			redirect: "/home",
-		},
-		{
-			name: "home",
-			path: "/home",
-			component: MainPage,
-		},
-		{
-			name: "contact",
-			path: "/contact",
-			component: ContactPage,
-		},
-		{
-			name: "offer",
-			path: "/offer",
-			component: OfferPage,
-		},
-	],
-	scrollBehavior(to, from, savedPosition) {
-		if (to.hash) {
-			return {
-				el: to.hash,
-				top: 78,
-				behavior: "smooth",
-			};
-		} else if (savedPosition) {
-			return savedPosition;
-		}
-		return {
-			left: 0,
-			top: 0,
-		};
-	},
+  history: createWebHistory(),
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: MainPage,
+    },
+    {
+      name: 'contact',
+      path: '/contact',
+      component: ContactPage,
+    },
+    {
+      name: 'offer',
+      path: '/offer',
+      component: OfferPage,
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 78,
+        behavior: 'smooth',
+      };
+    } else if (savedPosition) {
+      return savedPosition;
+    }
+    return {
+      left: 0,
+      top: 0,
+    };
+  },
 });
 
 const app = createApp(App);
 app.use(router);
-app.component("font-awesome-icon", FontAwesomeIcon);
-app.mount("#app");
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
